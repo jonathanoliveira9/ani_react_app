bef1a8333464e1937f871a724fe698cc9f5ad93d
Jonathan Silva <jonathan.silva.impulso@yapay.com.br>
2022-02-15 23:11:41 -0300

    exercise 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+const luana = {
+  client: 'Luana',
+  year: 27,
+  buy: [
+    { name: 'Notebook', price: 'R$ 2500' },
+    { name: 'Fridge', price: 'R$ 3000' },
+    { name: 'Smartphne', price: 'R$ 1500' },
+  ],
+  active: true,
+}
+
+const mario = {
+  client: 'Mario',
+  year: 31,
+  buy: [
+    { name: 'Notebook', price: 'R$ 2500' },
+    { name: 'Fridge', price: 'R$ 3000' },
+    { name: 'Smartphne', price: 'R$ 1500' },
+    { name: 'Guitar', price: 'R$ 3500' },
+  ],
+  active: false,
+}
 const App = () => {
-  return (
-    <div>My App</div>
-  );
+  const dados = luana;
+  const expend = dados.buy.map((a) =>
+    parseInt(a.price.replace('R$ ', ''))
+  ).reduce((a, b) => a + b)
+ return (
+   <div>
+     <p>Nome: {dados.client}</p>
+     <p>Idade: {dados.year}</p>
+     <p>Situação: <span style={{ color: dados.active ? 'green' : 'red' }}>{dados.active? 'Ativa' : 'Inativa'}</span></p>
+     <p>Total gasto: {expend}</p>
+     <p style={{display: expend > 10000 ? 'true' :'none'}}>Você está gastando muito.</p>
+   </div>
+
+ );
 }
 
 export default App;
