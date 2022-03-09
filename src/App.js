439db61e6c782a1d822bfbd65e1a12cd954d42e8
Jonathan de Oliveira Silva <jonathan.silva.impulso@yapay.com.br>
2022-03-09 14:25:47 -0300

    event

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  const products = [
-    {
-      id: 1,
-      name: 'Smartphone',
-      price: 'R$ 2000',
-      colors: ['#29d8d5', '#252a34', '#fc3766'],
-    },
-    {
-      id: 2,
-      name: 'Notebook',
-      price: 'R$ 3000',
-      colors: ['#ffd045', '#d4394b', '#f37c59'],
-    },
-    {
-      id: 3,
-      name: 'Tablet',
-      price: 'R$ 1500',
-      colors: ['#365069', '#47c1c8', '#f95786']
-    }
-  ]
-  return ( 
-    <section>
-      <div>
-        {products.filter((product) => ( parseInt(product['price'].replace('R$ ', '')) > 1500 )).map(({id, name, price, colors}) => (
-          <ul>
-            <h1>{name}</h1>
-            <p>Preço: R$ {price}</p>
-            {colors.map((color) =>(
-              <li key={color} style={{backgroundColor: color, color: 'white'}}>{color}</li>
-            ))} 
-          </ul>
-        ))}
-      </div>
-    </section>
+  function handleScroll(event){
+    console.log(event);
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  return (
+    <div style={{height: '200vh'}}>
+      <button onClick={(event) => console.log(event.target)}>Clique</button>
+    </div>
   );
 }
 
