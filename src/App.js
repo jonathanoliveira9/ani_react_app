ddd523c6955596835f6cb833cd63b145764b1b13
Jonathan de Oliveira Silva <jonathan.silva.impulso@yapay.com.br>
2022-06-01 13:33:05 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import Product from './Product';
-
+import Product from './components/Product'
 const App = () => {
-  const [data, setData] = React.useState(null)
-  const [loading, setLoading] = React.useState(null)
-  async function loadData (type) {
-    setLoading(true)
-    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${type}`)
-    setData(response)
-    const json = await response.json()
-    setData(json)
-    setLoading(false)
+  const [product, setProduct] = React.useState(null);
+  React.useEffect(() => {
+    const productLocal = window.localStorage.getItem('product')
+    if(productLocal != null) {
+      setProduct(productLocal)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (product != null) {
+      window.localStorage.setItem('product', product)
+    }
+  }, [product])
+  
+  function handleClick({target}) {
+   setProduct(target.innerText)
   }
+
+
   return (
     <div>
-      <button style={{ margin: '2px'}} onClick={() => loadData('tablet')}>tablet</button>
-      <button style={{ margin: '2px'}} onClick={() => loadData('smartphone')}>smartphone</button>
-      <button style={{ margin: '2px'}} onClick={() => loadData('notebook')}>notebook</button>
-      {loading && <p>Carregando</p>}
-      {data && <Product data={data}/>}
-     
+      <h1>Preferência: {product}</h1>
+      <button onClick={handleClick} style={{ marginRight: '1rem' }}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      <Product product={product}></Product>
     </div>
   );
 }
